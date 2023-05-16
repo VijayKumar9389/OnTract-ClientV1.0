85964b421b82a2cf04800572c6afcb0509f98002
Vijay Kumar <vijaykumar@MacBook-Pro.local>
2023-05-16 14:00:03 -0600

    testing

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,12 +3,14 @@ import './Home.scss';
 import StakeholderTable from '../../Components/Table/StakeholderTable/StakeholderTable';
 import Input from '../../Components/Input/Input';
 import FilterMenu from '../../Components/Filters/Filter';
+import Report from '../../Components/Reports/Report';
 
 function Home() {
 
     return (
         <div className='home-container'>
             <div className='home-body'>
+                <Report />
                 <FilterMenu isOpen={false} />
                 <Input />
                 <StakeholderTable />
